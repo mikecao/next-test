0abcadef3172fcd1499979a6bea127f5992b7150
Mike Cao <mike@mikecao.com>
2021-07-16 11:36:40 -0700

    Initial commit.

---
+++ pages/api/test.js
@@ -0,0 +1,13 @@
+import path from 'path';
+import maxmind from 'maxmind';
+import requestIp from 'request-ip';
+
+export default async function test(req, res) {
+  const ip = requestIp.getClientIp(req);
+
+  const lookup = await maxmind.open(path.resolve(__dirname, `../public/geo/GeoLite2-Country.mmdb`));
+
+  const result = lookup.get(ip);
+
+  res.status(200).json(result);
+};
