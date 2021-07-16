5911db3fbade876dd175bad768348caee088aa29
Mike Cao <mike@mikecao.com>
2021-07-16 12:09:46 -0700

    Get files.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import dirTree from 'directory-tree';
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe', filename: __filename, dirname: __dirname, cwd: process.cwd() })
+  res.status(200).json({ name: 'John Doe', filename: __filename, dirname: __dirname, cwd: process.cwd(), files: dirTree(process.cwd()) });
 }
