ea654d23241b48eaa1e2729356b7bbaa596f46f5
Mike Cao <mike@mikecao.com>
2021-07-20 15:41:58 -0700

    Add await.

--- pages/api/test.js
+++ pages/api/test.js
@@ -4,7 +4,7 @@ import { getCountry } from "../../lib/request";
 export default async function test(req, res) {
   const ip = requestIp.getClientIp(req);
 
-  const result = getCountry(req, ip);
+  const result = await getCountry(req, ip);
 
   res.status(200).json(result);
 };
